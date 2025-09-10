2f2a865be6653ef9665eecfee27f9da0a8bc10c1
Saulo Dias <saulo.dias@viajarbarato.com.br>
2025-09-10 16:54:34 -0300

    Aula 10

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,17 @@
+import { Hero } from "./hero.model";
+
+
+
+export const HEROES: Hero [] = [
+    {id:1, name: 'Wolverine'},
+    {id:2, name: 'Homem Aranha'},
+    {id:3, name: 'Batman'},
+    {id:4, name: 'Mulher Maravilha'},
+    {id:5, name: 'Capitão América'},
+    {id:6, name: 'Homem de Ferro'},
+    {id:7, name: 'Flash'},
+    {id:8, name: 'Thor'},
+    {id:9, name: 'Hulk'},
+    {id:10, name: 'Super Man'}
+  
+]
