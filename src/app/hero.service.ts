d1fd87b17f9d94429ac1a87ed4a93c453826af97
Saulo Dias <saulo.dias@viajarbarato.com.br>
2025-09-12 15:12:25 -0300

    Aula 14 - Observable

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero.model';
 import {HEROES} from './mock-heroes'
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  getHeroes() : Hero[]{
-    return HEROES;
+  getHeroes(): Observable<Hero[]>{
+    const heroes = of(HEROES); // o of é utilizado para transformar um array em um Observable
+    return heroes;
 }
 }
