21e58e4dfa1fbfde478adb81916f3d1d269605e1
Saulo Dias <saulo.dias@viajarbarato.com.br>
2025-09-18 11:39:38 -0300

    Aula 15 Observable a fundo

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero.model';
 import {HEROES} from './mock-heroes'
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +11,6 @@ export class HeroService {
   getHeroes(): Observable<Hero[]>{
     const heroes = of(HEROES); // o of é utilizado para transformar um array em um Observable
     return heroes;
+    
 }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero.model';
 import { HeroService } from '../hero.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-heroes',
@@ -11,15 +12,16 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor( private heroService: HeroService) {}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
-    this.getHeroes();   
+    this.getHeroes();
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);   
   }
+
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
