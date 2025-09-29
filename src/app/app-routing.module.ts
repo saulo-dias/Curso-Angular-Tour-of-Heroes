d15cdfc051cf91c7c92891104b9982f29ad0042c
Saulo Dias <saulo.dias@viajarbarato.com.br>
2025-09-29 11:24:25 -0300

    Aula 22 - Feature Module (Organizando os modules)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroDetailComponent } from './heroes/components/hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/components/heroes/heroes.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // Rota padrão redireciona para o dashboard
-  { path: 'heroes', component: HeroesComponent }, // Aqui definimos a rota para o componente HeroesComponent
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'heroes/:id', component: HeroDetailComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { CoreModule } from './core/core.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 
 
@@ -15,18 +14,21 @@ import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent, 
-    HeroDetailComponent, 
-    DashboardComponent
+    AppComponent
   ],
   imports: [
+    // Angular
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
-    CoreModule
+
+    // Feature
+    DashboardModule,
+    HeroesModule,
+
+    //App
+    CoreModule,
+    AppRoutingModule
     
   ],
   providers: [],
