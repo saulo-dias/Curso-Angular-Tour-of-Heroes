b7cee3dd88af5b276ffc82947137555fc4c8e126
Saulo Dias <saulo.dias@viajarbarato.com.br>
2025-10-03 11:16:45 -0300

    Aula 27 - Create Hero e acrecento botoes para create e update dos heroes

--- src/app/core/services/hero.service.ts
+++ src/app/core/services/hero.service.ts
@@ -32,15 +32,27 @@ export class HeroService {
     return this.http
       .get<Hero>(`${this.heroesUrl}/${id}`)
       .pipe(
-        tap((hero) => this.log(`fetched hero id=${id} and name=${hero.name}`))
+        tap((hero) => this.log(`fetched ${this.descAttributes(hero)}`))
       );
   }
+
+  //POST /heroes
+  create(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero).pipe(
+      tap((hero) => this.log(`created ${this.descAttributes(hero)}`))
+    );
+  }
+
 
   //PUT /heroes/:id
   update(hero: Hero): Observable<Hero> {
     return this.http.put<Hero>(`${this.heroesUrl}/${hero.id}`, hero).pipe(
-      tap((hero) => this.log(`updated hero id=${hero.id} and name=${hero.name}`))
+      tap((hero) => this.log(`updated ${this.descAttributes(hero)}`))
     );
+  }
+
+  private descAttributes(hero: Hero): string {
+    return `hero id=${hero.id} and name=${hero.name}`
   }
 
   private log(message: string): void {
--- src/app/heroes/components/hero-detail/hero-detail.component.ts
+++ src/app/heroes/components/hero-detail/hero-detail.component.ts
@@ -1,45 +1,54 @@
-import { Component, OnInit } from "@angular/core";
-import { Location } from "@angular/common";
-import { ActivatedRoute } from "@angular/router";
-import { Hero } from "../../../core/models/hero.model";
-import { HeroService } from "../../../core/services/hero.service";
-
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { Hero } from '../../../core/models/hero.model';
+import { HeroService } from '../../../core/services/hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.scss']
-
+  styleUrls: ['./hero-detail.component.scss'],
 })
 export class HeroDetailComponent implements OnInit {
   hero!: Hero;
+  isEditing!: boolean;
 
   constructor(
     private heroService: HeroService,
-     private location: Location, 
-     private route: ActivatedRoute
-    ){}
+    private location: Location,
+    private route: ActivatedRoute
+  ) {}
 
-    ngOnInit(): void {
-      this.getHero();
-        
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const paramId = this.route.snapshot.paramMap.get('id');
+
+    if (paramId === 'new') {
+      this.isEditing = false;
+      this.hero = {name: ''} as Hero;
+    } else {
+      this.isEditing = true;
+      const id = Number(paramId);
+      this.heroService.getOne(id).subscribe((hero) => (this.hero = hero));
     }
+  }
 
-    getHero(): void {
-      const id = Number(this.route.snapshot.paramMap.get('id'));
-      this.heroService.getOne(id).subscribe(hero => this.hero = hero);
-    }
+  goBack(): void {
+    this.location.back();
+  }
 
-    goBack(): void {
-      this.location.back();
-    }
+  create(): void {
+    this.heroService.create(this.hero).subscribe((hero) => this.goBack());
+  }
 
-    save(): void {
-      this.heroService.update(this.hero).subscribe((hero) => this.goBack());
-    }
+  update(): void {
+    this.heroService.update(this.hero).subscribe((hero) => this.goBack());
+  }
 
-    isFormValid(): boolean {
-      return !!this.hero.name.trim();
-    }
-
-} 
+  isFormValid(): boolean {
+    return !!this.hero.name.trim();
+  }
+}
